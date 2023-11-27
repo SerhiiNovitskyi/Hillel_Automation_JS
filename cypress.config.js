28d2eaec7e572760745d23aaebf5f7590c0825c7
SerhiiNovitskyi <sergi.novitskiy@gmail.com>
2023-11-27 15:34:07 +0200

    added_cypress_mochawesome_reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,19 @@
 import { defineConfig } from "cypress";
 import fs from "fs-extra";
 import { configurePlugin } from "cypress-mongodb";
+import lib from 'cypress-mochawesome-reporter/lib/index.js'
+
+const {beforeRunHook, afterRunHook} = lib
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'HTML reports created by AQA wizard',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   env: {
     newbornUrl: 'http://5.189.186.217',
     guruTestingUrl:'https://www.guru99.com',
@@ -35,65 +46,19 @@ export default defineConfig({
       }
       return LaunchOptions
     })
-    // configurePlugin(on)
-    // on('before:run', async (details) => {
-    //   console.log('override before:run');
-    //   await beforeRunHook(details);
-    // });
-
-    // on('after:run', async () => {
-    //   console.log('override after:run');
-    //   await afterRunHook();
-    // });
     configurePlugin(on)
     
+    on('before:run', async (details) => {
+      console.log('override before:run');
+      await beforeRunHook(details);
+    });
+
+    on('after:run', async () => {
+      console.log('override after:run');
+      await afterRunHook();
+    });
+
     return config
   },
 },
 })
-
-// const { defineConfig } = require('cypress')
-
-// module.exports = defineConfig({
-//   e2e: {
-//     experimentalStudio: true,
-//     viewportHeight: 900,
-//     viewportWidth: 1400,
-//     defaultCommandTimeout: 4000,
-//     // baseUrl: 'https://www.cypress.io',
-//     retries: {
-//       openMode: 1,
-//       runMode: 1,
-//     },
-//     // specPattern: 'cypress/e2e/myTests/*.cy.js',
-//     setupNodeEvents(on, config) {
-//       on('task', { log(message) { console.log(message); return null } })
-//       on('task', { saveUrl(url) { fs.writeFileSync('url.json', JSON.stringify(url)); return null } })
-//       const newUrl = config.env.urlFromCli || 'https://www.guru99.com'
-//       config.baseUrl = newUrl
-
-//       on("before:browser:launch", (browser, launchOptions) => {
-//         console.log(launchOptions.args);
-//         if (browser.name === "chrome") {
-//           launchOptions.args.push("--incognito");
-//         }
-//         return launchOptions;
-//       });
-
-//       configurePlugin(on)
-
-//       on('before:run', async (details) => {
-//         console.log('override before:run');
-//         await beforeRunHook(details);
-//       });
-
-//       on('after:run', async () => {
-//         console.log('override after:run');
-//         await afterRunHook();
-//       });
-
-//       return config
-//     },
-//   },
-// })
-
