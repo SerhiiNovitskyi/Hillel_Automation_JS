1d5fa67c40e62d097d334ed7f823a9e7754220fc
SerhiiNovitskyi <sergi.novitskiy@gmail.com>
2023-12-01 12:06:48 +0200

    HT#16 configure UI tests on playwright

---
+++ homework_playwright/playwright.config.js
@@ -0,0 +1,121 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
+
+
+
+// export default defineConfig({
+//   use: {
+//     // Whether to automatically download all the attachments.
+//     acceptDownloads: false,
+
+//     // An object containing additional HTTP headers to be sent with every request.
+//     extraHTTPHeaders: {
+//       'X-My-Header': 'value',
+//     },
+
+//     // Credentials for HTTP authentication.
+//     httpCredentials: {
+//       username: 'user',
+//       password: 'pass',
+//     },
+
+//     // Whether to ignore HTTPS errors during navigation.
+//     ignoreHTTPSErrors: true,
+
+//     // Whether to emulate network being offline.
+//     offline: true,
+
+//     // Proxy settings used for all pages in the test.
+//     proxy: {
+//       server: 'http://myproxy.com:3128',
+//       bypass: 'localhost',
+//     },
+
+//     // Capture screenshot after each test failure.
+//     screenshot: 'only-on-failure',
+
+//     // Record trace only when retrying a test for the first time.
+//     trace: 'on-first-retry',
+  
+//     // Record video only when retrying a test for the first time.
+//     video: 'on-first-retry'  
+
+//   },
+// });
+
